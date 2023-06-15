ae1427bb81c65c210487e95da3830d749ad45bea
mateo4002 <135989589+mateo4002@users.noreply.github.com>
2023-06-15 14:52:47 +0000

    implementacion de modulos

---
+++ ALGEBRA.py
@@ -0,0 +1,3 @@
+from math import sqrt as raiz
+def distancia(x1,y1,x2,y2):
+    return raiz((x2-x1)**2 + (y2-y1)**2)
---
+++ programa.py
@@ -0,0 +1,8 @@
+from MATEMATICAS import sumar, restar, multiplicar, dividir
+from ALGEBRA import distancia
+
+print (sumar(2,3))
+print (restar(5,2))
+print (multiplicar(5,5))
+print (dividir(10,2))
+print (distancia(1,2,3,4))
